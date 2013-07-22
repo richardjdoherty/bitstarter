d72dab1b272da11144df5bb44eab050579525bf9
Richard Doherty <richardjdoherty@gmail.com>
2013-07-22 22:51:12 +0000

    rolled back web.js

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var buffer = new buffer(30);
 
-var fs = require('fs');
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('reached break point1');
-    var buffer  = fs.readFileSync('index.html');
-    response.send(buffer);
-}
+  response.send('Hello World 2!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
