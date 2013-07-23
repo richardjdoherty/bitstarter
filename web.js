f51fd1cc311e27f23959748cbe778e5791307dda
Richard Doherty <richardjdoherty@gmail.com>
2013-07-23 00:07:36 +0000

    Modified to pull greeting from index.html

--- web.js
+++ web.js
@@ -1,21 +1,13 @@
 var fs = require('fs');
 
-var buf = new Buffer('Hello World from index.html!','utf-8');
+//var buf = new Buffer(1024);
 
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
-fs.readFileSync('index.html', function (err, buf) {
-  response.send('reached checkpoint');
-  if (err) throw err;
-  
-});
-
-
 app.get('/', function(request, response) {
-  response.send(new Buffer(buf.toString('utf8')));
+  response.send(fs.readFileSync('index.html').toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
