ef4af3220a42ea2ef0053b95a1877330ebbf4a74
Richard Doherty <richardjdoherty@gmail.com>
2013-07-24 16:59:23 +0000

    changed web.js to port 8080

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
-var fs = require('fs');
+/*  replace with code shown in HW4 as a new web.js
 
-//var buf = new Buffer(1024);
+var fs = require('fs');
 
 var express = require('express');
 var app = express();
@@ -10,9 +10,26 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString('utf8'));
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
+*/
+
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log("listening on " + port);
+});
 
